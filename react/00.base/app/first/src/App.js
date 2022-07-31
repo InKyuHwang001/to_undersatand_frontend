b564349dccfe6bf2d51423b0b5b4028a34acbd82
InGyu Hwang <hwanginkyu42@naver.com>
2022-07-31 17:25:20 +0900

    react 이벤트

--- react/00.base/app/first/src/App.js
+++ react/00.base/app/first/src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
   constructor (props){
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id:2,
+      welcome:{title:"welcome", desc:"Hello, React!!"},
       subject : { title : 'WEB' , sub : 'World Wide Web!'},
       contents : [
         {id : 1 , title : 'HTML' , desc : 'HTML is for information'},
@@ -18,15 +21,39 @@ class App extends Component {
     }
   };
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title=this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode==='read'){
+      var i =0;
+      while(i< this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc= data.desc;
+          break;
+        }
+        i=i+1;
+      }
+    }
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}>
         </Subject>
-        <Subject title='React' sub="For UI"></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <TOC data={this.state.contents} 
+          onChangePage={function(id){
+          this.setState({
+            mode:"read",
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}></TOC>
+        <Content title={_title} desc={_desc}></Content>
         <Footer title='hwang' sub="This is made by react"></Footer>
       </div>
     );
--- react/00.base/app/first/src/components/Subject.js
+++ react/00.base/app/first/src/components/Subject.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class Subject extends Component{
-    render() {
-      return (
-        <header>
-          <h1>{this.props.title}</h1>
-          {this.props.sub}
-        </header>
-      );
-    }
+class Subject extends Component {
+  render() {
+    return ( 
+      <header>
+        <h1><a href="/" onClick={function(e){
+           e.preventDefault();
+           this.props.onChangePage();
+         }.bind(this) } >{this.props.title}</a></h1>
+        {this.props.sub}
+      </header>
+    );
   }
+}
 
 export default Subject;
