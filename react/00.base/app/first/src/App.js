9e97917b6c700c5522cc5d5c051bbe9dbdcdf564
InGyu Hwang <hwanginkyu42@naver.com>
2022-08-01 14:03:46 +0900

    create

--- react/00.base/app/first/src/App.js
+++ react/00.base/app/first/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
 import TOC from "./components/TOC"
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
 import Subject from './components/Subject';
 import Footer from './components/Footer';
+import Control from './components/Control';
+import CreateContent from './components/CreateContent';
 
 class App extends Component {
   constructor (props){
     super(props);
+    this.max_content_id=3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id:2,
       welcome:{title:"welcome", desc:"Hello, React!!"},
       subject : { title : 'WEB' , sub : 'World Wide Web!'},
@@ -21,10 +24,11 @@ class App extends Component {
     }
   };
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title=this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article=<ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode==='read'){
       var i =0;
       while(i< this.state.contents.length){
@@ -32,10 +36,25 @@ class App extends Component {
         if(data.id === this.state.selected_content_id){
           _title = data.title;
           _desc= data.desc;
+          _article=<ReadContent title={_title} desc={_desc}></ReadContent>
           break;
         }
         i=i+1;
       }
+    } else if (this.state.mode ==='create'){
+      _article=<CreateContent onSubmit={function(_title, _desc){
+        //add content to this.state.contents
+        this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //   {id : this.max_content_id, title :_title , desc :_desc}
+        // );성능 개선시 똥임
+        var _contents= this.state.contents.concat(
+          {id : this.max_content_id, title :_title , desc :_desc}
+        )
+        this.setState({
+          contents: _contents
+        }); 
+      }.bind(this)} ></CreateContent>
     }
     return (
       <div className="App">
@@ -53,7 +72,12 @@ class App extends Component {
             selected_content_id:Number(id)
           });
         }.bind(this)}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
+        {_article}
         <Footer title='hwang' sub="This is made by react"></Footer>
       </div>
     );
---
+++ react/00.base/app/first/src/components/Control.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Control extends Component {
+  render() {
+    return ( 
+      <ul>
+          <li><a href="/create" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('create');
+          }.bind(this)}>create</a></li>
+          <li><a href="/update" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('update');
+          }.bind(this)}>update</a></li>
+          {/* <li><a href="/delete">delete</a></li> : 미리가서 하지도 않은 삭재해버릴수 */}
+          <li><input onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('create');
+          }.bind(this)} type="button" value="delete"></input></li>
+        </ul>
+    );
+  }
+}
+
+export default Control;
